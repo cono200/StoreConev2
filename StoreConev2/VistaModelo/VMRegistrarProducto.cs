e36703276f5ff026347fee947c53c29ea2b9029b
cono200 <tatattian04@gmail.com>
2024-02-20 21:27:07 -0700

    diselo y noritifcacionesadas efkjbfewshijpf

---
+++ StoreConev2/VistaModelo/VMRegistrarProducto.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using StoreConev2.VistaModelo;
+using StoreConev2.Vistas;
+
+namespace StoreConev2.VistaModelo
+{
+    public class VMRegistrarProducto : BaseViewModel
+    {
+        #region VARIABLES
+        string _Texto;
+
+        #endregion
+        #region CONSTRUCTOR
+        public VMRegistrarProducto(INavigation navigation)
+        {
+            Navigation = navigation;
+
+        }
+        #endregion
+        #region OBJETOS
+        public string Texto
+        {
+            get { return _Texto; }
+            set { SetValue(ref _Texto, value); }
+        }
+        #endregion
+        #region PROCESOS
+        public async Task ProcesoAsyncrono()
+        {
+
+        }
+        public async  Task IrNotificaciones()
+        {
+            await Navigation.PushAsync( new Notificaciones());
+        }
+        public void procesoSimple()
+        {
+
+        }
+        #endregion
+        #region COMANDOS
+        public ICommand ProcesoAsyncomand => new Command(async () => await ProcesoAsyncrono());
+        public ICommand IrNotificacionescomand => new Command(async () => await IrNotificaciones());
+        public ICommand ProcesoSimpcomand => new Command(procesoSimple);
+        #endregion
+    }
+}
--- StoreConev2/Vistas/Notificaciones.xaml.cs
+++ StoreConev2/Vistas/Notificaciones.xaml.cs
@@ -1,21 +1,21 @@
-﻿using StoreConev2.VistaModelo;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StoreConev2.VistaModelo;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace StoreConev2.Vistas
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class RegistrarProducto : ContentPage
+    public partial class Notificaciones : ContentPage
     {
-        public RegistrarProducto()
+        public Notificaciones()
         {
             InitializeComponent();
-            BindingContext = new VMRegistrarProducto(Navigation);
+            //BindingContext = new VMRegistrarProducto(Navigation);
         }
     }
 }
