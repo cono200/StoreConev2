3688ea6efc08928213f1320b09b3c78a68084c9d
LDVP5049 <luismaster752@gmail.com>
2024-03-08 14:47:24 -0700

    MErmasModel

---
+++ StoreConev2/Modelo/Merma.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StoreConev2.Modelo
+{
+    public class Merma
+    {
+        public long Codigo { get; set; }
+        public string Tipo_de_merma { get; set; }
+        public DateTime? Fecha_ingreso { get; set; }
+        public string Nombre_producto { get; set; } 
+    }
+}
