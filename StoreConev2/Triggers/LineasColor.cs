66b6d3a4f6ee1e317c8ada3b2e0ed50dd2d13c32
PC <danyel.16@gmail.com>
2024-03-11 22:43:20 -0600

    ListaCorregida

---
+++ StoreConev2/Triggers/LineasColor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace StoreConev2.Triggers
+{
+    public class LineasColor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var action = (string)value;
+            switch (action)
+            {
+                case "Registro":
+                    return Color.LightGreen;
+                case "Mermas":
+                    return Color.LightBlue;
+                case "Eliminación":
+                    return Color.LightSalmon;
+                default:
+                    return Color.Gray;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
